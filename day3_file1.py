53230cc406328f042b1f8e13fce1d424e25dc5d6
IvanKasibwa <ivanagesa@gmail.com>
2023-08-04 20:33:52 +0300

    added file 1 and 2

---
+++ day3_file1.py
@@ -0,0 +1,10 @@
+print("Hello, world!")  # Line 1
+print("This is an example of Python code.")  # Line 2
+print("I am learning programming.")  # Line 3
+print("Python is a great language for beginners.")  # Line 4
+print("I will continue practicing and improving my skills.")  # Line 5
+print("Coding is fun!")  # Line 6
+print("I can also print numbers:", 5)  # Line 7
+print("The sum of 3 and 7 is:", 3 + 7)  # Line 8
+print("Variables are used to store values.", "For example:", "x = 10")  # Line 9
+print("I hope this example helps you understand Python better.")  # Line 10
---
+++ day3_file2.py
@@ -0,0 +1,12 @@
+# File 2 - Another example Python code with print statement
+
+print("Welcome to my program!")  # Line 1
+print("In this program, we will calculate the area of a rectangle.")  # Line 2
+length = 5  # Line 3
+width = 7  # Line 4
+area = length * width  # Line 5
+print("Length:", length)  # Line 6
+print("Width:", width)  # Line 7
+print("Area:", area)  # Line 8
+print("The area of the rectangle with length", length, "and width", width, "is", area)  # Line 9
+print("Thank you for using my program!")  # Line 10
