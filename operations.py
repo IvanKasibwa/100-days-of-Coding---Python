52c7ad23c644f87c8d97cff0ca9b69b9760dd7f3
IvanKasibwa <ivanagesa@gmail.com>
2023-08-04 21:43:02 +0300

    operations

---
+++ operations.py
@@ -0,0 +1,32 @@
+#Today you will try doing maths on python
+var_1=11
+var_2=2
+
+#print the variables
+print("var_1 =",var_1)
+print("var_2 =",var_2)
+
+#addition
+sum=var_1+var_2
+print("var_1+var_2 =",sum)
+
+#subtraction
+difference=var_1-var_2
+print("var_1-var_2 =",difference)
+
+#multiplication
+product=var_1*var_2
+print("var_1*var_2 =",product)
+
+#division
+quotient=var_1/var_2
+print("var_1/var_2 =",quotient)
+
+#modulus
+remainder=var_1%var_2
+print("var_1%var_2 =",remainder)
+
+#power
+exponent=var_1**var_2
+print("var_1**var_2 =",exponent)
+
